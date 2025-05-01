401641cf666e7de2fa5416018ce59d693a3bf947
SofiaTejerina <sofiatejerina23@gmail.com>
2025-05-01 11:46:27 -0300

    Use mdx nextjs package to display md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+
 import Markdown from "markdown-to-jsx";
-import content from "src/content/valves/index.md"
+import Welcome from "../content/valves/index.md"
 
 export default function Page() {
     return <>
         <h1>Hello, Next.js!</h1>
-        <Markdown>{content}</Markdown>
+        <Welcome/>
     </>
 }
