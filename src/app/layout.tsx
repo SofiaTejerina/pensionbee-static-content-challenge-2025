6d145386ad319a06bf81bd6e628acf1d4c10bfc8
SofiaTejerina <sofiatejerina23@gmail.com>
2025-05-04 20:12:48 -0300

    update code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import './globals.css'
+import "./globals.css";
 
-export default function RootLayout({children}: {
-    children: React.ReactNode
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html>
-        <head>
-            <title>Welcome to Acme</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;400;800&display=swap"
-                  rel="stylesheet"/>
-        </head>
-        <body>
-        {children}
-        </body>
-        </html>
-    )
+  return (
+    <html>
+      <head>
+        <title>Welcome to Acme</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;400;800&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
