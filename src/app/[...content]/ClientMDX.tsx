42b34ad1b66778b6558ae91e7220959e982eff74
SofiaTejerina <sofiatejerina23@gmail.com>
2025-05-04 12:14:01 -0300

    Add doc to code

--- src/app/[...content]/ClientMDX.tsx
+++ src/app/[...content]/ClientMDX.tsx
@@ -6,6 +6,7 @@ import {loadMDX} from "./mdxLoader";
 import {useMDXComponents} from "../mdx-components";
 import {MDXProvider} from "@mdx-js/react";
 
+// have a separated component because catch all dynamic segments should be in a server component while mdx nextJS package should be used in a client component
 export default function ClientMDX({path}: { path: string }) {
     const components = useMDXComponents({})
     const Post = useMemo(
